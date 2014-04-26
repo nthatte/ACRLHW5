aad03be4bd571f6cfd978dd747eea6af6edbabf8
Vishnu Desaraju <rajeswar@cmu.edu>
2014-04-26 17:11:44 -0400

    fixed in_polygon

--- src/in_polygon.py
+++ src/in_polygon.py
@@ -1,9 +1,9 @@
-import shapely
+from shapely.geometry import Point, Polygon
 
 def in_polygon(points, polygon_corners):
-    polygon = shapely.geometry.Polygon(polygon_corners)
+    polygon = Polygon(polygon_corners)
     for point in points:
-        shapely_point = shapely.geometry.Point(point)
-        if polygon.intersects(point):
-            return true 
-    return false
+        shapely_point = Point(point)
+        if polygon.intersects(shapely_point):
+            return True 
+    return False
